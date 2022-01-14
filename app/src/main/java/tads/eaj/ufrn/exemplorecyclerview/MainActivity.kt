35922d921ee53bb7c6d86e0f176f3f39d610f9a8
taniro <tanirocr@gmail.com>
2022-01-14 14:57:32 -0300

    update 2022

--- app/src/main/java/tads/eaj/ufrn/exemplorecyclerview/MainActivity.kt
+++ app/src/main/java/tads/eaj/ufrn/exemplorecyclerview/MainActivity.kt
@@ -3,20 +3,23 @@ package tads.eaj.ufrn.exemplorecyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.room.Room
-import kotlinx.android.synthetic.main.activity_main.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import android.widget.Toast
 import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import jp.wasabeef.recyclerview.animators.FlipInTopXAnimator
 import jp.wasabeef.recyclerview.animators.LandingAnimator
 import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
+import tads.eaj.ufrn.exemplorecyclerview.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
 
     val db:AppDatabase by lazy {
         Room.databaseBuilder(this, AppDatabase::class.java, "frutas.sqlite")
@@ -26,7 +29,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
 
         val f1 = Fruta ("Maça", R.drawable.fruit)
         val f2 = Fruta ("Pera", R.drawable.fruit)
@@ -45,14 +49,14 @@ class MainActivity : AppCompatActivity() {
         val adapter = NovoFrutaAdapter()
         adapter.frutas = listaFrutas
 
-        recyclerview.adapter = adapter
+        binding.recyclerview.adapter = adapter
 
-        val layout = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true)
+        val layout = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         //val layout = StaggeredGridLayoutManager(10, StaggeredGridLayoutManager.HORIZONTAL)
 
-        recyclerview.layoutManager = layout
+        binding.recyclerview.layoutManager = layout
 
-        recyclerview.addOnItemTouchListener(NovoRecyclerViewClickListener(this, recyclerview,
+        binding.recyclerview.addOnItemTouchListener(NovoRecyclerViewClickListener(this, binding.recyclerview,
             object : NovoRecyclerViewClickListener.onItemClickListener{
                 override fun onItemClick(v: View, position: Int) {
                     Toast.makeText(this@MainActivity, "Clique simples", Toast.LENGTH_SHORT)
@@ -62,24 +66,24 @@ class MainActivity : AppCompatActivity() {
                 override fun onItemLongClick(v: View, position: Int) {
                     val removida = listaFrutas[position]
                     listaFrutas.remove(removida)
-                    recyclerview.adapter!!.notifyItemRemoved(position)
+                    binding.recyclerview.adapter!!.notifyItemRemoved(position)
                     Toast.makeText(this@MainActivity, "Clique longo", Toast.LENGTH_SHORT)
                         .show()
                     val snack = Snackbar.make(
-                        recyclerview.parent as View,"Removido",Snackbar.LENGTH_LONG )
+                        binding.recyclerview.parent as View,"Removido",Snackbar.LENGTH_LONG )
                         .setAction("Cancelar") {
                             listaFrutas.add(position, removida)
-                            recyclerview.adapter!!.notifyItemInserted(position)
+                            binding.recyclerview.adapter!!.notifyItemInserted(position)
                         }
                     snack.show()
                 }
             }))
 
         /*
-        recyclerview.addOnItemTouchListener(
+        binding.recyclerview.addOnItemTouchListener(
             MyRecyclerViewClickListener(
                 this@MainActivity,
-                recyclerview,
+                binding.recyclerview,
                 object : MyRecyclerViewClickListener.OnItemClickListener {
                     override fun onItemClick(view: View, position: Int) {
                         Toast.makeText(this@MainActivity, "Clique simples", Toast.LENGTH_SHORT)
@@ -89,14 +93,14 @@ class MainActivity : AppCompatActivity() {
                     override fun onItemLongClick(view: View, position: Int) {
                         val removida = listaFrutas[position]
                         listaFrutas.remove(removida)
-                        recyclerview.adapter!!.notifyItemRemoved(position)
+                        binding.recyclerview.adapter!!.notifyItemRemoved(position)
                         Toast.makeText(this@MainActivity, "Clique longo", Toast.LENGTH_SHORT)
                             .show()
                         val snack = Snackbar.make(
-                            recyclerview.parent as View,"Removido",Snackbar.LENGTH_LONG )
+                            binding.recyclerview.parent as View,"Removido",Snackbar.LENGTH_LONG )
                             .setAction("Cancelar") {
                                 listaFrutas.add(position, removida)
-                                recyclerview.adapter!!.notifyItemInserted(position)
+                                binding.recyclerview.adapter!!.notifyItemInserted(position)
                             }
                         snack.show()
                     }
@@ -106,8 +110,8 @@ class MainActivity : AppCompatActivity() {
          */
 
 
-        //recyclerview.itemAnimator = DefaultItemAnimator()
-        //recyclerview.itemAnimator = LandingAnimator()
-        //recyclerview.itemAnimator = FlipInTopXAnimator()
+        //binding.recyclerview.itemAnimator = DefaultItemAnimator()
+        //binding.recyclerview.itemAnimator = LandingAnimator()
+        //binding.recyclerview.itemAnimator = FlipInTopXAnimator()
     }
 }
